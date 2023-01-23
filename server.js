956e9e76d584cdf199dced90eacdcc86de3c3ec0
oh-ve <maricic.ve@gmail.com>
2023-01-23 17:34:20 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.send("Workers of the world, unite!");
+});
+
+app.listen(PORT, () => {
+  console.log(`The revolution is happening on http://localhost:${PORT}`);
+});
